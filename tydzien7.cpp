7bda8c1538059cd3078fa183c7a8ac99ecce9eef
Paweł Bąk <92019664+nolmo12@users.noreply.github.com>
2021-12-01 18:30:07 +0100

    Update tydzien7.cpp

--- tydzien7.cpp
+++ tydzien7.cpp
@@ -1,146 +1,141 @@
-// ConsoleApplication39.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+// nauka.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "pch.h"
 #include <iostream>
 #include <vector>
+#include "nauka.h"
 
-void wypisz(int w)
+void wypisz(int a)
 {
-	std::cout << w << "\n";
+    std::cout << a << "\n";
 }
 void wypisz(std::vector<int>tab)
 {
-	for (int i = 0; i < tab.size(); i++)
-	{
-		std::cout << tab[i] << " ";
-	}
-	std::cout << "\n";
+    for (int i = 0; i < tab.size(); i++)
+    {
+        std::cout << tab[i] << " ";
+    }
 }
-bool czyNalezy(std::vector<int>tab, int element)
+void wypisz(std::vector<std::vector<int>>tab)
 {
-	for (int i = 0; i < tab.size(); i++)
-	{
-		if (tab[i] == element)
-		{
-			return true;
-		}
-	}
-	return false;
+    for (int i = 0; i < tab[0].size(); i++)
+    {
+        std::cout << tab[0][i] << " ";
+        std::cout << tab[1][i] << " ";
+    }
+}
+int min(std::vector<int>tab)
+{
+    int a = tab[0];
+    for (int i = 0; i < tab.size(); i++)
+    {
+        if (tab[i] < a)
+        {
+            a = tab[i];
+        }
+    }
+    return a;
+}
+int max(std::vector<int>tab)
+{
+    int a = tab[0];
+    for (int i = 0; i < tab.size(); i++)
+    {
+        if (tab[i] > a)
+        {
+            a = tab[i];
+        }
+    }
+    return a;
+}
+bool czyNalezy(int element, std::vector<int>tab)
+{
+    for (int i = 0; i < tab.size(); i++)
+    {
+        if (tab[i] == element)
+            return true;
+    }
+    return false;
 }
 bool czyZawiera(std::vector<int>tab, std::vector<int>podzbior)
 {
-	for (int i = 0; i < podzbior.size(); i++)
-	{
-		if (!czyNalezy(tab, podzbior[i]))
-		{
-			return false;
-		}
-	}
-	return true;
-}
-int Min(std::vector<int>tab)
-{
-	int a;
-	a = tab[0];
-	for (int i = 0; i < tab.size(); i++)
-	{
-		if (tab[i] < a)
-		{
-			a = tab[i];
-		}
-	}
-	return a;
-}
-int Max(std::vector<int>tab)
-{
-	int a;
-	a = tab[0];
-	for (int i = 0; i < tab.size(); i++)
-	{
-		if (tab[i] > a)
-		{
-			a = tab[i];
-		}
-	}
-	return a;
-}
-int suma(int a, int b)
-{
-	return a + b;
+    for (int i = 0; i < podzbior.size(); i++)
+    {
+        if (!czyNalezy(podzbior[i], tab))
+            return false;
+    }
+    return true;
 }
 std::vector<int>unikalny(std::vector<int>tab)
 {
-	std::vector<int>wynik;
-	for (int i = 0; i < tab.size(); i++)
-	{
-		if (!czyNalezy(wynik, tab[i]))
-		{
-			wynik.push_back(tab[i]);
-		}
-	}
-	return wynik;
+    std::vector<int>wynik;
+    for (int i = 0; i < tab.size(); i++)
+    {
+        if (!czyNalezy(tab[i], wynik))
+        {
+            wynik.push_back(tab[i]);
+        }
+    }
+    return wynik;
 }
 
-std::vector < std::vector<int>>czestosc(std::vector<int>tab)
+std::vector<std::vector<int>>ileWystapien(std::vector<int>tab)
 {
-	std::vector<std::vector<int>>wynik;
-	std::vector<int>u=unikalny(tab);
-	std::vector<int>c;
-	int licznik = 0;
-	for (int i = 0; i < u.size(); i++)
-	{
-		int element = u[i];
-		for (int j = 0; j < tab.size(); j++)
-		{
-			if (element == tab[j])
-			{
-				licznik++;
-			}
-		}
-		c.push_back(licznik);
-	}
-	wynik.push_back(u);
-	wynik.push_back(c);
-	return wynik;
+    std::vector<std::vector<int>>wynik;
+    std::vector<int>liczby = unikalny(tab);
+    std::vector<int>zliczone;
+    int licznik=0;
+    int element;
+    for (int i = 0; i < liczby.size(); i++)
+    {
+        element = liczby[i];
+        for(int j=0;j<tab.size();j++)
+            if (element == tab[j])
+            {
+                licznik++;
+            }
+        zliczone.push_back(licznik);
+        licznik = 0;
+    }
+    wynik.push_back(liczby);
+    wynik.push_back(zliczone);
+    return wynik;
 }
 
 int main()
 {
-	int element;
-	std::cin >> element;
-	std::vector<int>tab = { 8, 3, 1, 8, 2, 5, 3, 1, 8 };
-	std::vector<int>tab2 = { 4,5,8 };
-	std::vector<int>dom = { 1,2,5,7,8,9,11 };
-	wypisz(tab);
-	if (czyNalezy(tab, element))
-	{
-		std::cout << "tak\n";
-	}
-	else
-	{
-		std::cout << "Nie\n";
-	}
-	if (czyZawiera(tab, tab2))
-	{
-		std::cout << "tak2\n";
-	}
-	else
-	{
-		std::cout << "nie2\n";
-	}
-	wypisz(Min(dom));
-	wypisz(Max(dom));
-	wypisz(unikalny(tab));
+    int w = 5;
+    std::vector<int>tab = { 4,4,4,5,3,3,4,7,4};
+    std::vector<int>tab2 = {4,5,3};
+    wypisz(w);
+    wypisz(tab);
+    wypisz(min(tab));
+    wypisz(max(tab));
+    wypisz(unikalny(tab));
+    std::cout << "\n";
+    wypisz(ileWystapien(tab));
+    std::cout << "\n";
+   if (czyNalezy(3, tab))
+    {
+        std::cout << "tak \n";
+    }
+    else
+        std::cout << "nie \n";
+    if (czyZawiera(tab, tab2))
+    {
+        std::cout << "Zawiera \n";
+    }
+    else
+        std::cout << "Nie zawiera \n";
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
 
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
